24953b03036b2ac328c59627daa5815fc2664769
Niyati Gahoi <122302695+Niyati1312Gahoi@users.noreply.github.com>
2024-06-18 22:55:18 +0530

    Time: 2 ms (82.18%) | Memory: 44.9 MB (84.16%) - LeetSync

--- 944-smallest-range-i/smallest-range-i.java
+++ 944-smallest-range-i/smallest-range-i.java
@@ -3,8 +3,12 @@ class Solution {
         int min=nums[0];
         int max=nums[0];
         for(int i=0;i<nums.length;i++){
-            max=Math.max(max,nums[i]);
-            min=Math.min(min,nums[i]);
+            if(max<nums[i]){
+                max=nums[i];
+            }
+            if(min>nums[i]){
+                min=nums[i];
+            }
         }
         if((min+k)>=(max-k)){
             return 0;
