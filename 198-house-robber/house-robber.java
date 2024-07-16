3f66c65ecdcaeb0442a347e1afab70425cb77fc3
Niyati Gahoi <122302695+Niyati1312Gahoi@users.noreply.github.com>
2024-07-16 14:59:24 +0530

    Time: 0 ms (100.00%) | Memory: 41 MB (25.31%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -2,7 +2,7 @@ class Solution {
     public int rob(int[] nums) {
         int[] dp=new int[nums.length];
         Arrays.fill(dp,-1);
-        return maxmoney(nums,0,dp);
+        return maxmoney(nums,nums.length-1,dp);
 
 
         
@@ -10,16 +10,17 @@ class Solution {
      public static int maxmoney(int[] house,int index,int[] dp){
 
 
-        if(index>= house.length){
+        if(index < 0){
             return 0;
         }
         if(dp[index]!=-1){
             return dp[index];
         }
-        int choose=house[index]+maxmoney(house,index+2,dp);
-        int not_choose=maxmoney(house,index+1,dp);
+        int choose=house[index]+maxmoney(house,index-2,dp);
+        int not_choose=maxmoney(house,index-1,dp);
 
         return dp[index]=Math.max(choose,not_choose);
 
     }
+
 }
