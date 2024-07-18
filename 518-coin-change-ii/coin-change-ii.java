c582f91368598764e07c9ea376abbe48a48f8aa0
Niyati Gahoi <122302695+Niyati1312Gahoi@users.noreply.github.com>
2024-07-18 14:58:23 +0530

    Time: 10 ms (23.55%) | Memory: 46.2 MB (68.74%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.java
+++ 518-coin-change-ii/coin-change-ii.java
@@ -1,31 +1,27 @@
 class Solution {
     public int change(int amount, int[] coins) {
-        int[][] dp=new int[amount+1][coins.length];
-        for(int[]arr:dp){
-            Arrays.fill(arr,-1);
-        }
-        return change(coins,amount,0,dp);
-
-        
-    }
-     public static int change(int[] arr,int target,int index,int[][] dp){
-        if(target==0){
-            return 1;
-        }
        
-        if(index == arr.length){
-            return 0;
-        }
-         if(dp[target][index]!=-1){
-            return dp[target][index];
-        }
-        int choose,not_choose;
-        choose=not_choose=0;
-        if(arr[index]<= target){
-            choose=change(arr,target-arr[index],index,dp);
+        return change(coins,amount,coins.length);
+
+
+    }
+    public static int change(int[] arr,int target,int index){
+        int[][] dp=new int[target+1][arr.length+1];
+        Arrays.fill(dp[0],1);
+        for(int amount=1;amount<=target;amount++){
+            for(int j=1;j<=arr.length;j++){
+                int coin=arr[j-1];
+                int pick=0,not_pick=0;
+                if(coin<=amount){
+                    pick=dp[amount-coin][j];
+
+                }
+                not_pick=dp[amount][j-1];
+                dp[amount][j]=pick+not_pick;
+            }
+            
 
         }
-        not_choose=change(arr,target,index+1,dp);
-        return dp[target][index]=choose+not_choose;
+        return dp[target][index];
     }
 }
