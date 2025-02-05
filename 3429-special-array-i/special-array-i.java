1a8bc4c918995f903152eb6b9ca64de4bd1be682
Niyati Gahoi <122302695+Niyati1312Gahoi@users.noreply.github.com>
2025-02-05 23:03:21 +0530

    Time: 1 ms (32.79%) | Memory: 43.3 MB (55.73%) - LeetSync

--- 3429-special-array-i/special-array-i.java
+++ 3429-special-array-i/special-array-i.java
@@ -3,15 +3,13 @@ class Solution {
         if(nums.length==1){
             return true;
         }
-        int c=0;
+     
         for(int i=1;i<nums.length;i++){
-            if((nums[i]%2==0 && nums[i-1]%2!=0)||(nums[i-1]%2==0 && nums[i]%2!=0) ){
-                c++;
-            }
+           if(((nums[i]^nums[i-1]) & 1)==0){
+            return false;
+           }
         }
-        if(c==nums.length-1){
-            return true;
-        }
-       return false; 
+      
+       return true; 
     }
 }
