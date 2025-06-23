cccad361c80b93f80c25b2fbbaf234f64900c54c
Niyati Gahoi <122302695+Niyati1312Gahoi@users.noreply.github.com>
2025-06-23 23:35:40 +0530

    Time: 653 ms (10.93%) | Memory: 43.4 MB (50.78%) - LeetSync

---
+++ 611-valid-triangle-number/valid-triangle-number.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int triangleNumber(int[] nums) {
+        Arrays.sort(nums);
+        int c=0;
+        for(int i=0;i<nums.length-2;i++){
+            for(int j=i+1;j<nums.length-1;j++){
+                for(int k=j+1;k<nums.length;k++){
+                    if((nums[i]+nums[j]>nums[k])){
+                        c++;
+                    }else{
+                        break;
+                    }
+                }
+            }
+        }
+        return c;
+    }
+}
