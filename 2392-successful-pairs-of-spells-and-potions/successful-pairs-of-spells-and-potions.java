1e76849aa8ed0dcad800f9fc2c9fba173bae5dd6
Niyati Gahoi <122302695+Niyati1312Gahoi@users.noreply.github.com>
2025-06-24 00:25:33 +0530

    Time: 47 ms (46.91%) | Memory: 65.1 MB (52.32%) - LeetSync

--- 2392-successful-pairs-of-spells-and-potions/successful-pairs-of-spells-and-potions.java
+++ 2392-successful-pairs-of-spells-and-potions/successful-pairs-of-spells-and-potions.java
@@ -4,16 +4,16 @@ class Solution {
         int[] arr=new int[spells.length];
        
         for(int i=0;i<spells.length;i++){
-            int l=0,h=potions.length;
+            int l=0,h=potions.length-1;
             
 
-            while(l<h){
+            while(l<=h){
                 int mid=(l+h)/2;
                 if((long)spells[i]*potions[mid]<success){
                     l=mid+1;
                 }else if((long)spells[i]*potions[mid]>=success){
                  
-                  h=mid;
+                  h=mid-1;
 
                 }
             }
